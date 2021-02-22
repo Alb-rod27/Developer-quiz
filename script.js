c95dbe9fb514024ee336392709939a1b5c96bf04
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-02-22 11:20:12 -0800

    added questions array and altered start function

--- script.js
+++ script.js
@@ -5,10 +5,29 @@ var startBtn = document.querySelector("#start");
 
 
 var timerId;
+var timeLeft = 60;
+var currentQuestionIndex = 0; 
+
+var questions = [
+  { q: "What is the purpose of HTML?", a: "styling", b: "functionality", c: "structure", d: "none of these"},
+  { q: "What is the purpose of CSS?", a: "structure", b: "styling", c: "emailing", d: "phone calls"},
+  { q: "In HTML what tag would you use to add the biggest type of section heading?", a: "<h2>", b: "<h3>", c: "<h1>", d: "<head>"},
+  { q: "What does HTML stand for?", a: "Ham Tomato Mayo and Lettuce", b: "HyperText Markup Language", c: "How To Mail Letters", d: "Hate To Make Lemonade"},
+  { q: "What is the purpose of Javascript?", a: "To make coffee", b: "functionality", c: "to make a movie", d: "structure"}
+]
 
 
+function startQuiz() {
 
-function clockTick() {
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+
+  questionsEl.removeAttribute("class");
+
+  timerEl.textContent = time;
+}
+
+function countDown() {
   
   time--;
   timerEl.textContent = time;
