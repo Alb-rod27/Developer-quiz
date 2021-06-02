2175dac855d2a0fdb413ab8abbd9a6574bd19856
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-06-01 20:21:23 -0700

    reworking script.js

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+let currQuestion = 0;
+let question = [
+  {
+    question: 'What is the purpose of HTML?',
+    choiceA:'Styling',
+    choiceB:'functionality',
+    choiceC:'Structure',
+    correctAnswer: 'C'
+  },
+  {
+    question:'What is the purpose of CSS?',
+    choiceA:'Emails',
+    choiceB:'Styling',
+    choiceC:'Structure',
+    correctAnswer: 'B'
+  },
+  {
+    question:'In HTML what tag would you use to add the biggest type of section heading?',
+    choiceA:'<h2>',
+    choiceB:'<head>',
+    choiceC:'<h1>',
+    correctAnswer: 'C'
+  },
+  {
+    question:'What does HTML stand for?',
+    choiceA:'HyperText Markup Language',
+    choiceB:'Ham Tomato Mayo and Lettuce',
+    choiceC:'How To Mail Letters',
+    correctAnswer: 'A'
+  },
+  {
+    question:'What is the purpose of Javascript?',
+    choiceA:'To make coffee',
+    choiceB:'functionality',
+    choiceC:'structure',
+    correctAnswer: 'B'
+  },
+]
+
+let timeLeft = 0;
+
+let score = 0;
+
+//start of quiz
