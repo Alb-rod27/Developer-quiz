d3748a70503f11f05d4b046c8a83bcdec090c462
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-06-01 22:23:18 -0700

    test working

--- assets/script.js
+++ assets/script.js
@@ -128,3 +128,66 @@ function finishGame() {
   document.getElementById('timer').classList.add('hide');
   document.getElementById('timer').classList.add('hide');
 };
+
+function submitScore() {
+  if (name.value === '' || null) {
+      window.alert('Please enter your initials!')
+  } else {
+      localStorage.setItem("player-initials", document.getElementById('name').value,
+      localStorage.setItem("player-score", JSON.stringify(score)))
+  };
+  submitBtn.onclick = location.href = './highscores.html';
+};
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => { currentQuestionsIndex++; setNextQuestion() });
+finishButton.addEventListener('click', finishGame);
+submitBtn.addEventListener('click', submitScore);
+
+const questions = [
+  {
+    question: 'What is the purpose of HTML?',
+    answers: [
+      { text: 'styling', correct: false},
+      { text: 'functionality', correct: false},
+      { text: 'structure', correct: true},
+      { text: 'none of these', correct: false}
+    ]
+  },
+  {
+    question: 'What is the purpose of CSS?',
+    answers: [
+      { text: 'structure', correct: false},
+      { text: 'styling', correct: true},
+      { text: 'emailing', correct: false},
+      { text: 'phone calls', correct: false}
+    ]
+  },
+  {
+    question: 'In HTML what tag would you use to add the biggest type of section heading?',
+    answers: [
+      { text: '<h2>', correct: false},
+      { text: '<h3>', correct: false},
+      { text: '<h1>', correct: true},
+      { text: '<head>', correct: false}
+    ]
+  },
+  {
+    question: 'What does HTML stand for?',
+    answers: [
+      { text: 'Ham Tomato Mayo and Lettuce', correct: false},
+      { text: 'HyperText Markup Language', correct: true},
+      { text: 'How To Mail Letters', correct: false},
+      { text: 'Hate To Make Lemonade', correct: false}
+    ]
+  },
+  {
+    question: 'What is the purpose of Javascript?',
+    answers: [
+      { text: 'To make coffee', correct: false},
+      { text: 'functionality', correct: true},
+      { text: 'to make a movie', correct: false},
+      { text: 'structure', correct: false}
+    ]
+  },
+];
